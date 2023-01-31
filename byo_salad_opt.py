05ab9aacb71da36fcbb3bd0a057ca69426e99b00
Stephen Polcyn <stephen@reverielabs.com>
2023-01-31 11:27:04 -0500

    Run Black and update sweetgreen info

--- byo_salad_opt.py
+++ byo_salad_opt.py
@@ -1,16 +1,17 @@
 import pulp
 import pandas as pd
 
+
 def byo_salad_model(P, C, F):
     # import nutritional csv
-    menu = pd.read_csv('./sweetgreen_PP.csv')
-    menu = menu.dropna().reset_index(drop = True)
+    menu = pd.read_csv("./sweetgreen_PP.csv")
+    menu = menu.dropna().reset_index(drop=True)
 
     # set solver
-    solver = pulp.getSolver('GUROBI')
+    solver = pulp.getSolver("GUROBI")
 
     # pull out the build your own ingredients
-    ingredients = menu[menu['Category'] != 'SALADS'].copy()
+    ingredients = menu[menu["Category"] != "SALADS"].copy()
 
     # Instantiate the model
     model = pulp.LpProblem("MacroModel", pulp.LpMinimize)
@@ -19,56 +20,57 @@ def byo_salad_model(P, C, F):
     indices = list(zip(ingredients.Category, ingredients.Name))
 
     # Set decision variables as Binary 0 or 1
-    x = pulp.LpVariable.dicts("x", indices, cat='Binary')
+    x = pulp.LpVariable.dicts("x", indices, cat="Binary")
 
     # Build objective function
-    zp = dict(zip(indices, ingredients['Protein (g)']))  # protein
-    zc = dict(zip(indices, ingredients['Total Carbs (g)']))  # carbs
-    zf = dict(zip(indices, ingredients['Total Fat (g)']))  # fats
-    zfiber = dict(zip(indices, ingredients['Fiber (g)']))  # fiber
-    zsodium = dict(zip(indices, ingredients['Sodium (mg)']))  # sodium
-    zsugar = dict(zip(indices, ingredients['Sugars (g)']))  # sugar
+    zp = dict(zip(indices, ingredients["Protein (g)"]))  # protein
+    zc = dict(zip(indices, ingredients["Total Carbs (g)"]))  # carbs
+    zf = dict(zip(indices, ingredients["Total Fat (g)"]))  # fats
+    zfiber = dict(zip(indices, ingredients["Fiber (g)"]))  # fiber
+    zsodium = dict(zip(indices, ingredients["Sodium (mg)"]))  # sodium
+    zsugar = dict(zip(indices, ingredients["Sugars (g)"]))  # sugar
 
     # - zfiber[i] + zsugar[i] + zsodium[i] / 100
     # Set Objective Function:
     model += (P + C + F) - pulp.lpSum([(zp[i] + zc[i] + zf[i]) * x[i] for i in indices])
 
     # Add Constraints
-    model += pulp.lpSum([(x[i] * zp[i]) for i in indices]) <= P, 'Protein Max'
-    model += pulp.lpSum([(x[i] * zc[i]) for i in indices]) <= C, 'Carb Max'
-    model += pulp.lpSum([(x[i] * zf[i]) for i in indices]) <= F, 'Fat Max'
+    model += pulp.lpSum([(x[i] * zp[i]) for i in indices]) <= P, "Protein Max"
+    model += pulp.lpSum([(x[i] * zc[i]) for i in indices]) <= C, "Carb Max"
+    model += pulp.lpSum([(x[i] * zf[i]) for i in indices]) <= F, "Fat Max"
 
-    br = [(k[0],k[1]) for k, v in x.items() if k[0] == 'BREAD']
-    model += pulp.lpSum([x[(i,j)] for i,j in br]) <= 1, 'Bread Limit'
+    br = [(k[0], k[1]) for k, v in x.items() if k[0] == "BREAD"]
+    model += pulp.lpSum([x[(i, j)] for i, j in br]) <= 1, "Bread Limit"
 
-    bs = [(k[0], k[1]) for k, v in x.items() if k[0] == 'BASES']
-    model += pulp.lpSum([x[(i, j)] for i, j in bs]) <= 2, 'Base max'
-    model += pulp.lpSum([x[(i, j)] for i, j in bs]) >= 1, 'Base min'
+    bs = [(k[0], k[1]) for k, v in x.items() if k[0] == "BASES"]
+    model += pulp.lpSum([x[(i, j)] for i, j in bs]) <= 2, "Base max"
+    model += pulp.lpSum([x[(i, j)] for i, j in bs]) >= 1, "Base min"
 
-    ing = [(k[0], k[1]) for k, v in x.items() if k[0] == 'INGREDIENTS']
-    model += pulp.lpSum([x[(i, j)] for i, j in ing]) <= 4, 'Ingredient Limit'
+    ing = [(k[0], k[1]) for k, v in x.items() if k[0] == "INGREDIENTS"]
+    model += pulp.lpSum([x[(i, j)] for i, j in ing]) <= 6, "Ingredient Limit"
 
-    pr = [(k[0], k[1]) for k, v in x.items() if k[0] == 'PREMIUMS']
-    model += pulp.lpSum([x[(i, j)] for i, j in pr]) <= 2, 'Premium Limit'
+    pr = [(k[0], k[1]) for k, v in x.items() if k[0] == "PREMIUMS"]
+    model += pulp.lpSum([x[(i, j)] for i, j in pr]) <= 1, "Premium Limit"
 
-    dr = [(k[0], k[1]) for k, v in x.items() if k[0] == 'DRESSINGS']
-    model += pulp.lpSum([x[(i, j)] for i, j in dr]) <= 2, 'Dressing Limit'
+    dr = [(k[0], k[1]) for k, v in x.items() if k[0] == "DRESSINGS"]
+    model += pulp.lpSum([x[(i, j)] for i, j in dr]) <= 2, "Dressing Limit"
 
-    bv = [(k[0], k[1]) for k, v in x.items() if k[0] == 'BEVERAGES']
-    model += pulp.lpSum([x[(i, j)] for i, j in bv]) <= 1, 'Beverages Limit'
+    bv = [(k[0], k[1]) for k, v in x.items() if k[0] == "BEVERAGES"]
+    model += pulp.lpSum([x[(i, j)] for i, j in bv]) <= 1, "Beverages Limit"
 
     return model
 
+
 def byo_postprocess(model):
-    #Print Status
+    # Print Status
     print(pulp.LpStatus[model.status])
 
-    #Print decision variables and get name of chosen ingredients
+    # Print decision variables and get name of chosen ingredients
     solution = list()
     for v in model.variables():
-        if(v.varValue > 0):
+        if v.varValue > 0:
             print(v.name, "=", v.varValue)
-            name = v.name.split('x_')[1].replace("_", " ")
+            name = v.name.split("x_")[1].replace("_", " ")
             name = name.split("', '")[1]
             name = name.split("')")[0]
             solution += [name]
--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from premade_salad_opt import *
 from byo_salad_opt import *
 from full_menu_model import *
 
-if __name__ == '__main__':
-    #import nutritional csv
-    menu = pd.read_csv('./sweetgreen_PP.csv')
+if __name__ == "__main__":
+    # import nutritional csv
+    menu = pd.read_csv("./sweetgreen_PP.csv")
     menu = menu.dropna()
 
-    #set solver
-    solver = pulp.getSolver('GUROBI')
+    # set solver
+    solver = pulp.getSolver("GUROBI")
 
-    #Set Macro limits
+    # Set Macro limits
     P = 67
     C = 120
     F = 87
 
-    #run premade salad model
+    # run premade salad model
     premade_model = premadesalad_selection(P, C, F)
 
     # Solve model
     # set solver
-    solver = pulp.getSolver('GUROBI')
+    solver = pulp.getSolver("GUROBI")
 
     premade_model.solve(solver)
 
-    #postprocess pm solution
+    # postprocess pm solution
     premade_solution = pm_postprocess(premade_model)
 
     # run build your own BYO salad model
@@ -38,30 +38,34 @@ if __name__ == '__main__':
     # postprocess pm solution
     byo_solution = byo_postprocess(byo_model)
 
-    #Full Model
+    # Full Model
     full_model = full_menu_model(P, C, F)
     full_model.solve(solver)
     full_solution = full_postprocess(full_model)
-    if full_solution == 'BYO':
+    if full_solution == "BYO":
         solution = byo_solution
     else:
-        solution = premade_solution
+        solution = [premade_solution]
 
     # Parse out the Overall solution then condense it down to one salad
-    result = menu[menu['Name'].isin(solution)].copy()
+    result = menu[menu["Name"].isin(solution)].copy()
     condensed_result = pd.DataFrame(result.iloc[:, 2:].sum()).T
-    rP = condensed_result['Protein (g)'].sum()
-    rC = condensed_result['Total Carbs (g)'].sum()
-    rF = condensed_result['Total Fat (g)'].sum()
+    rP = condensed_result["Protein (g)"].sum()
+    rC = condensed_result["Total Carbs (g)"].sum()
+    rF = condensed_result["Total Fat (g)"].sum()
 
-    print('Final Salad:', solution)
+    print("Final Salad:", solution)
 
-    print("Target Protein: {}  "
-          "Salad Protein: {}  "
-          "Remaining Protein: {}".format(P, rP, P - rP))
-    print("Target Carbs: {}  "
-          "Salad Carbs: {}  "
-          "Remaining Carbs: {}".format(C, rC, C - rC))
-    print("Target Fat: {}  "
-          "Salad Fat: {}  "
-          "Remaining Fat: {}".format(F, rF, F - rF))
+    print(
+        "Target Protein: {}  "
+        "Salad Protein: {}  "
+        "Remaining Protein: {}".format(P, rP, P - rP)
+    )
+    print(
+        "Target Carbs: {}  "
+        "Salad Carbs: {}  "
+        "Remaining Carbs: {}".format(C, rC, C - rC)
+    )
+    print(
+        "Target Fat: {}  " "Salad Fat: {}  " "Remaining Fat: {}".format(F, rF, F - rF)
+    )
